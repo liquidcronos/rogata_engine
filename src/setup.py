761f9fc436f7795f3c44df18d49668d95a886ec9
Jan Baumgärnter <22688144+liquidcronos@users.noreply.github.com>
2021-08-03 08:17:44 +0200

    added requirements to setup file
    this allows auto installation of required python libraries

--- src/setup.py
+++ src/setup.py
@@ -1,10 +1,16 @@
 from setuptools import setup, find_packages
+
+
+with open("requirements.txt", "r") as fh:
+    requirements = fh.readlines()
+
 setup(name='rogata_library',
 version='0.9',
 description='Robotic Games Tabletop Engine',
 url='https://rogata-engine.readthedocs.io/en/latest/what_is_rogata.html',
 author='Jan Baumgärtner',
-author_email='author@email.com',
+author_email='jan.baumgaertner@ziti.uni-heidelberg.de',
 license='MIT',
 packages=['rogata_library'],
+install_requires=[req for req in requirements if req[:2] != "# "],
 zip_safe=False)
