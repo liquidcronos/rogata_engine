904902a692fa61322002dd365ade8a5acd22e4cc
Jan Baumgärnter <22688144+liquidcronos@users.noreply.github.com>
2021-08-03 08:50:03 +0200

    add simple test object

--- tests/test_game_object.py
+++ tests/test_game_object.py
@@ -1,11 +1,16 @@
 import rogata_library as rgt
+import numpy as np
 import unittest
 
-
-
+height = 100
+width  = 100
+outer_contour = np.array([[0,0],[0,height],[width,height],[width,0]], dtype=np.int32)
+inner_contour = np.array([[0,0],[0,height/10.0],[width/10.0,height/10.0],[width/10.0,0]], dtype=np.int32)
+test_object   = rgt.game_object("test_object",[outer_contour,inner_contour],np.array([1]))
 
 
 # Test basic functionality
+#name (test name not string (should throw error))
 #get_position
 #is_inside  (mind edge case of the point being on the line)
 #line_intersect (test required precision, test if no interesction, test not normalized direction)
