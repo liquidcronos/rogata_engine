9f577aa26508c477d0b29a9b2d0fde86fafb5b9c
Jan Baumgärnter <22688144+liquidcronos@users.noreply.github.com>
2021-08-03 08:28:59 +0200

    add basic descriptions for tests
    This is a setup for the final test structure

---
+++ tests/test_game_object.py
@@ -0,0 +1,18 @@
+import rogata_library as rgt
+import unittest
+
+
+
+
+
+# Test basic functionality
+#get_position
+#is_inside  (mind edge case of the point being on the line)
+#line_intersect (test required precision, test if no interesction, test not normalized direction)
+#move_object     (test new position, test rotation, test rotation angle with different conventions (larger than pi, -pi))
+#shortest_distance (test distance inside,test distance outside, test point on object border, )
+
+
+# Test validity of object hierachy
+
+
