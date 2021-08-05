58ffb7ad25fe477a23c81c2d766e1b60e0ae6d54
Jan Baumgärtner <22688144+liquidcronos@users.noreply.github.com>
2021-08-05 09:41:29 +0200

    made setup.py be installed in catkin_make

--- setup.py
+++ setup.py
@@ -1,9 +1,18 @@
-from setuptools import setup, find_packages
+#!/usr/bin/env python
 
+from distutils.core import setup
+from catkin_pkg.python_setup import generate_distutils_setup
 
+setup_args = generate_distutils_setup(
+     packages=['rogata_engine'],
+     package_dir={'': 'src'}
+)
+
+setup(**setup_args)
+
+'''
 with open("requirements.txt", "r") as fh:
     requirements = fh.readlines()
-
 setup(name='rogata_library',
 version='0.9',
 description='Robotic Games Tabletop Engine',
@@ -14,3 +23,4 @@ license='MIT',
 packages=['rogata_library'],
 install_requires=[req for req in requirements if req[:2] != "# "],
 zip_safe=False)
+'''
